ebf51cc69c4138724db7ec44ce15ecab8823bcc8
alicanerdurmaz <alicanerdurmaz@gmail.com>
2019-05-11 00:12:09 +0300

    v1

--- test.py
+++ test.py
@@ -1,32 +1,44 @@
 import re
 import passwords
 from robobrowser import RoboBrowser
+from bs4 import BeautifulSoup
+import requests
 import time
 
 br = RoboBrowser()
 
-
-for i in range(0, 52):
+# if pasworlds.len < 2. program not running.
+# i dont know why.
+for x in range(0, passwords.len):
 
     # time.sleep(3)
     br.open("http://www.vproce.net/OgrenciBilgiSistemi/default.aspx")
 
     form = br.get_form(action='./default.aspx')
-    form['txtusername'] = passwords.OGR_PASS[i]
+    form['txtusername'] = passwords.OGR_PASS[x]
     # time.sleep(3)
     br.submit_form(form)
 
-    br.open("http://www.vproce.net/OgrenciBilgiSistemi/dersbilgileri.aspx?dersno=1")
+    temp = str(br.parsed())
+    firstPage = BeautifulSoup(temp, 'lxml')
+    for i in firstPage.findAll("div", {"id": "DersKodu"}):
+        if(i.text == "MAT1010"):
+            tempLink = i.parent
+            linka = str(tempLink)
+            break
+
+    finalLink = "http://www.vproce.net/OgrenciBilgiSistemi/" + linka[9:36]
+
+    br.open(finalLink)
     # time.sleep(3)
 
     src = str(br.parsed())
 
     match = re.search(r'<div class="not">(.*?)</div>', src)
     if match:
-        print(passwords.OGR_PASS[i] + " = " + match.group(1))
+        print(passwords.OGR_PASS[x] + " = " + match.group(1))
+        # print(match.group(1))
         file1 = open("puanlar.txt", "a")
         file1.write(str(match.group(1)))
         file1.write('\n')
         file1.close()
-
-    # time.sleep(3)
